8316fd6e07b3f2ee9ba8ead0a8a6376375118b0c
raulsistemasji <raul.sistemasj@gmail.com>
2021-12-09 21:45:27 -0300

    ajuste url

--- service_user_auth/database/mongoose.js
+++ service_user_auth/database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb+srv://mbafiap:fiap101210@cluster0.qpnsf.mongodb.net/projetomicroservicelogin?retryWrites=true&w=majority"
+const url = "ADICIONAR URL DO MONGODB"
 
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
 mongoose.Promise = global.Promise;
